70ddaf672a53fbf37c0f38ea4e9ede175cbe8a79
VandersliceApril-FS <acvanderslice@student.fullsail.edu>
2021-07-22 09:13:09 -0500

    Ref #9 The new item now displays in the html however the modal doesn't close so that is the next thing to work on. I am preventing default behavior so maybe that has something to do with it.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 class Main{
     constructor() {
-        // let addButton = document.querySelector('#add-item-btn');
-        // addButton.addEventListener("click", this.getData);
+        // when a new list is created, add it to this array to access later.
+        this.lists = [];
+        this.currentList = [];
+
+        this.formModal = document.querySelector('#add-item-modal');
         let form = document.querySelector('form');
         form.addEventListener('submit', e => e.preventDefault());
         
         let submitBtn = form.querySelector('#modal-submit-btn');
         submitBtn.addEventListener("click", e => this.createListItem());
+
+        
     }
     
 
@@ -17,6 +22,35 @@ class Main{
         newItem.store = document.querySelector('#item-store').value;
         newItem.quantity = Number(document.querySelector('#item-quantity').value);
         console.log(newItem);
+        
+        // items wil be added to the listObject array
+        this.currentList.push(newItem);
+        this.displayListItems(this.currentList);
+        document.querySelector('form').reset();
+        let myModal = document.getElementById('#add-item-modal');
+        // myModal.hide();
+        console.log(`Current List: ${this.currentList}`);
+        
+    }
+
+    displayListItems(arr){
+        let mainList = document.querySelector('#list-container');
+        arr.forEach(item => {
+            let htmlToAdd = 
+        `
+        <li class="list-group-item">
+            <div class="row">
+                <h2  class="col">${item.name}</h2>
+                <h3 class="col-3">${item.cost}</h3>
+            </div>
+            <p>${item.store}</p>
+        
+            <p class="col">${item.quantity}</p>
+            <button class="col-3" id="more">...</button>
+        </li>
+        `;
+        mainList.insertAdjacentHTML('afterbegin', htmlToAdd);
+        })  
     }
 
     
