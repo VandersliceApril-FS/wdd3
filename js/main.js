d451613f06e20fafc08ad8fe639329f9b957cfe8
VandersliceApril-FS <acvanderslice@student.fullsail.edu>
2021-07-23 13:42:08 -0500

    Trying out saving to local storage.

--- js/main.js
+++ js/main.js
@@ -3,52 +3,103 @@ class Main{
         // when a new list is created, add it to this array to access later.
         this.lists = [];
         this.currentList = [];
+        this.listContainer = document.querySelector('#list-container');
 
         this.formModal = document.querySelector('#add-item-modal');
         let form = document.querySelector('form');
         // form.addEventListener('submit', e => e.preventDefault());
+        let clearBtn = document.querySelector('#clear-list-btn');
+        clearBtn.addEventListener("click", e => this.clearList());
         
         let submitBtn = form.querySelector('#modal-submit-btn');
         submitBtn.addEventListener("click", e => this.createListItem());
 
+        const myStorage = window.localStorage
+        
+        this.savedList = Object.keys(localStorage);
         
     }
-    
+
+    getFromLocal(arr){
+        arr.forEach(item => {
+            const str = localStorage.getItem(item);
+
+            let parsedObj = JSON.parse(str);
+            this.savedList.push(parsedObj);
+        });
+
+        this.displayListItem(this.savedList);
+        
+    }
+
+    saveToLocal(itemObject){
+        let newItemObject = JSON.stringify(itemObject);
+        localStorage.setItem(newItemObject, `${newItemObject.name}`);
+    }
 
     createListItem(){
+        // create a new list item object
         let newItem = new ListItemDO();
+
+        //gather the data from the form
         newItem.name = document.querySelector('#item-name').value;
-        newItem.cost = Number(document.querySelector('#item-cost').value);
+        newItem.cost = Number(document.querySelector('#item-cost').value).toFixed(2);
         newItem.store = document.querySelector('#item-store').value;
         newItem.quantity = Number(document.querySelector('#item-quantity').value);
-        console.log(newItem);
         
-        // items wil be added to the listObject array
+        
+        // add the item to the currentList
         this.currentList.push(newItem);
-        this.displayListItem(newItem);
+
+        // save the item to local storage
+        this.saveToLocal(newItem);
+
+        // call the function to display items in the html
+        this.displayListItem(this.currentList);
+
+
         document.querySelector('form').reset();
-        console.log(`Current List: ${this.currentList}`);
-        
+        this.savedList.forEach(item => {
+            console.log(`Added Item: \rlocal storage: ${item.name}`);
+        });
     }
 
-    displayListItem(item){
-        let mainList = document.querySelector('#list-container');
+    resetListContainer(){
+        this.listContainer.innerHTML = "";
+    }
+
+
+    displayListItem(arr){
+        // reset list container to avoid duplicates
+        this.resetListContainer();
         
+        // loop through the array and display each item in the html
+        arr.forEach(item => {
         let htmlToAdd = 
         `
         <li class="list-group-item">
             <div class="row">
-                <h2  class="col">${item.name}</h2>
-                <h3 class="col-3">${item.cost}</h3>
+                <p  class="col">${item.name}</p>
+                <p class="col-3">$ ${item.cost}</p>
             </div>
             <p>${item.store}</p>
         
-            <p class="col">${item.quantity}</p>
+            <p class="col">Qty: ${item.quantity}</p>
             <button class="col-3" id="more">...</button>
         </li>
         `;
-        mainList.insertAdjacentHTML('afterbegin', htmlToAdd);
+        this.listContainer.insertAdjacentHTML('afterbegin', htmlToAdd);    
+        }); 
     }
+
+    clearList(e){
+        localStorage.clear();
+        window.location.reload();
+        this.savedList.forEach(item => {
+            console.log(`Cleared List\r\nlocal storage: ${item.name}`);
+        });
+    }
+
 
     
 }
